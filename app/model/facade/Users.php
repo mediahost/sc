2455e5f4d118718594a4afaa9e07c50d6695b5fc
Petr Poupě <petr.poupe@gmail.com>
2014-08-19 13:10:54 +0200

    Install - init users

--- app/model/facade/Users.php
+++ app/model/facade/Users.php
@@ -5,53 +5,54 @@ namespace App\Model\Facade;
 use App\Model\Entity\User,
 	Kdyby\Doctrine\EntityDao;
 
-
 class Users extends Base
 {
-	
+
 	/** @var EntityDao */
 	private $users;
-	
+
 	protected function init()
 	{
 		$this->users = $this->em->getDao(User::getClassName());
 	}
 
 	/**
-     * 
-     * @param type $email
-     * @return User
-     */
-    public function findByEmail($email)
-    {
-        return $this->users->findOneBy(['email' => $email]);
-    }
-    
-    /**
-     * Check if email is unique
-     * @param type $email
-     * @return bool
-     */
-    public function isUnique($email)
-    {
-        return $this->findByEmail($email) === NULL;
-    }
-    
-    /**
-     * Create user if isnt exists
-     * @param type $email
-     * @param type $password
-     * @return \App\Model\Entity\User|null
-     */
-    public function create($email, $password)
-    {
-        if ($this->findByEmail($email) === NULL) { // check unique
-            $user = new User;
-            $user->email = $email;
-            $user->password = $password;
-            return $this->users->save($user); // ToDo: Delete this line
-        }
-        return NULL;
-    }
+	 * 
+	 * @param type $email
+	 * @return User
+	 */
+	public function findByEmail($email)
+	{
+		return $this->users->findOneBy(['email' => $email]);
+	}
+
+	/**
+	 * Check if email is unique
+	 * @param type $email
+	 * @return bool
+	 */
+	public function isUnique($email)
+	{
+		return $this->findByEmail($email) === NULL;
+	}
+
+	/**
+	 * Create user if isnt exists
+	 * @param type $email
+	 * @param type $password
+	 * @return \App\Model\Entity\User|null
+	 */
+	public function create($email, $password, Role $role)
+	{
+		if ($this->findByEmail($email) === NULL) { // check unique
+			$user = new User;
+			$user->email = $email;
+			// TODO: pro nastavení nového aplikačního přihlášení dodělej fci: $user->auth(login, pass)
+			$user->addRole($role);
+			// TODO: ukládat - zakomentováno, dokud neexistuje $user->auth()
+//			return $this->users->save($user); // ToDo: Delete this line
+		}
+		return NULL;
+	}
 
 }
