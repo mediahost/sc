d26af29694a0a24fb23b4faebdca22d7989b451a
martinsifra <me@martinsifra.cz>
2014-08-19 12:32:29 +0200

    Repaired InstallPresented

--- app/module/FrontModule/presenters/InstallPresenter.php
+++ app/module/FrontModule/presenters/InstallPresenter.php
@@ -6,53 +6,57 @@ use Nette;
 use Tracy\Debugger as Debug;
 
 /**
- * Deny presenter.
+ * Install presenter.
  */
 class InstallPresenter extends BasePresenter
 {
 
-    /** @var string */
-    private $tempDir;
+	/** @var string */
+	private $tempDir;
 
-    /** @var string */
-    private $wwwDir;
+	/** @var string */
+	private $wwwDir;
 
-    /** @var string */
-    private $installDir;
+	/** @var string */
+	private $installDir;
 
-    public function setPathes($tempDir, $wwwDir)
-    {
-        $this->tempDir = $tempDir;
-        $this->wwwDir = $wwwDir;
-        $this->installDir = $this->tempDir . "/install";
-    }
+	public function __construct() {
+		parent::__construct();
+	}
+	
+	public function setPathes($tempDir, $wwwDir)
+	{
+		$this->tempDir = $tempDir;
+		$this->wwwDir = $wwwDir;
+		$this->installDir = $this->tempDir . "/install";
+	}
 
-    protected function startup()
-    {
-        parent::startup();
-        if (!is_dir($this->installDir)) {
-            mkdir($this->installDir);
-        }
-    }
+	protected function startup()
+	{
+		parent::startup();
+		if (!is_dir($this->installDir)) {
+			mkdir($this->installDir);
+		}
+	}
 
-    public function actionDefault()
-    {
-        $this->installAdminer();
-        $this->terminate();
-    }
+	public function actionDefault()
+	{
+		$this->installAdminer();
+		$this->terminate();
+	}
 
-    private function installAdminer()
-    {
-        $lockFile = $this->installDir . "/adminer";
-        if (!file_exists($lockFile)) {
-            chmod($this->wwwDir . "/adminer/database.sql", 0777);
-            $this->lockFile($lockFile);
-        }
-    }
+	private function installAdminer()
+	{
+		$lockFile = $this->installDir . "/adminer";
+		if (!file_exists($lockFile)) {
+			chmod($this->wwwDir . "/adminer/database.sql", 0777);
+			$this->lockFile($lockFile);
+		}
+	}
 
-    private function lockFile($lockFile)
-    {
-        file_put_contents($lockFile, "1");
-    }
+	private function lockFile($lockFile)
+	{
+		file_put_contents($lockFile, "1");
+	}
 
 }
