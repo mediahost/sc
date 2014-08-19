1a179eee30aeb9af616056d09e4ff0dddaca77fb
martinsifra <me@martinsifra.cz>
2014-08-19 13:59:25 +0200

    UserFacade

--- app/model/facade/Users.php
+++ app/model/facade/Users.php
@@ -6,17 +6,16 @@ use App\Model\Entity\User,
 	App\Model\Entity,
 	Kdyby\Doctrine\EntityDao;
 
-
 class Users extends Base
 {
-	
+
 	/** @var EntityDao */
 	private $users;
 	
 	/** @var EntityDao */
 	private $roles;
-	
-	
+
+		
 	protected function init()
 	{
 		$this->users = $this->em->getDao(User::getClassName());
@@ -32,7 +31,7 @@ class Users extends Base
 	{
 		return $this->users->findOneBy(['email' => $email]);
 	}
-	
+
 	/**
 	 * Check if email is unique
 	 * @param type $email
@@ -42,20 +41,27 @@ class Users extends Base
 	{
 		return $this->findByEmail($email) === NULL;
 	}
-	
+
 	/**
 	 * Create user if isnt exists
 	 * @param type $email
 	 * @param type $password
 	 * @return \App\Model\Entity\User|null
 	 */
-	public function create($email, $password)
+	public function create($email, $password, Role $role)
 	{
 		if ($this->findByEmail($email) === NULL) { // check unique
 			$user = new User;
 			$user->email = $email;
-			$user->password = $password;
-			return $this->users->save($user); // ToDo: Delete this line, maybe not
+			
+			$auth = new Entity\Auth();
+			$auth->key = $email;
+			$auth->source = 'app';
+			$auth->hash = $password;
+			
+			$user->addRole($role);
+			
+			return $this->users->save($user);
 		}
 		return NULL;
 	}
@@ -64,15 +70,13 @@ class Users extends Base
 	/**
 	 * 
 	 * @param Entity\User $user
-	 * @param Entity\Role|int|string $role
+	 * @param Entity\Role|string $role
 	 */
 	public function addRole(Entity\User $user, $role)
 	{
 		if (!($user instanceof Entity\Role)) {
 			if (is_string($role)) {
 				$role = $this->roles->findOneBy(['name' => $role]);
-			} elseif (is_int($role)) {
-				$role = $this->roles->find($role);
 			} elseif (is_array($role)) {
 				$role = $this->roles->findBy(['name' => $role]);
 			} else {
