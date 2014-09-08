35a67554aabadfbae3ae26de34eb7423b17b7569
martinsifra <me@martinsifra.cz>
2014-09-08 14:14:29 +0200

    Removing local files (test.bat, tests\config.local.neon)

--- bin/test.bat
+++
@@ -1,8 +0,0 @@
-set this=%CD%
-set root=%this%/..
-set phpIni="c:\Program Files (x86)\EasyPHP-DevServer-13.1VC9\binaries\php\php_runningversion"
-set testDir="%root%/tests"
-set testLog=%root%/tests/test.log
-cd /d "%root%/vendor/bin"
-start tester.bat -c %phpIni% -log %testLog% -w %testDir% %testDir%
-cd /d "%this%"
