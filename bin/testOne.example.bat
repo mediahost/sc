bf98cfc201222ea6a019ab898dee0ac62f81933e
Samo Lisý <g.vvoody@gmail.com>
2014-10-21 15:40:36 +0200

    bat file for testing one test case

---
+++ bin/testOne.example.bat
@@ -0,0 +1,9 @@
+set this=%CD%
+set root=%this%/..
+set phpIni="c:/xampp/php/php.ini"
+set testDir="%root%/tests/"
+set testFile="%root%/tests/model/UsersTests.phpt"
+set testLog=%root%/tests/test.log
+cd /d "%root%/vendor/bin"
+start tester.bat -c %phpIni% -log %testLog% --stop-on-fail -w %testDir% %testFile%
+cd /d "%this%"
