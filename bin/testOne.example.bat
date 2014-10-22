71bc82c104ecf1a83edfdd743735f11a54874aef
Petr Poupě <petr.poupe@gmail.com>
2014-10-22 11:19:33 +0200

    removing test UsersTests.phpt

--- bin/testOne.example.bat
+++ bin/testOne.example.bat
@@ -2,7 +2,7 @@ set this=%CD%
 set root=%this%/..
 set phpIni="c:/xampp/php/php.ini"
 set testDir="%root%/tests/"
-set testFile="%root%/tests/model/UsersTests.phpt"
+set testFile="%root%/tests/model/installer/Installer.phpt"
 set testLog=%root%/tests/test.log
 cd /d "%root%/vendor/bin"
 start tester.bat -c %phpIni% -log %testLog% --stop-on-fail -w %testDir% %testFile%
--- tests/model/installer/Installer.phpt
+++ tests/model/installer/Installer.phpt
@@ -6,7 +6,7 @@ use Nette,
 	Tester,
 	Tester\Assert;
 
-$container = require __DIR__ . '/../bootstrap.php';
+$container = require __DIR__ . '/../../bootstrap.php';
 
 /**
  * TEST: Installer Testing
