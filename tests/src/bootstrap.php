635ead16055edb2675bb3afe15d2bf80c3dd1d52
Petr Poupě <petr.poupe@gmail.com>
2014-10-29 13:29:05 +0100

    move tests src to folder src

--- tests/src/bootstrap.php
+++ tests/src/bootstrap.php
@@ -3,35 +3,34 @@
 // Increase default timeot limit because of locks on DB.
 set_time_limit(180);
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../../vendor/autoload.php';
 
 if (!class_exists('Tester\Assert')) {
 	echo "Install Nette Tester using `composer update --dev`\n";
 	exit(1);
 }
 
-// Using app temporary directory
-define('TEMP_DIR', __DIR__ . '/temp/');
+define('TEMP_DIR', __DIR__ . '/../temp/');
 
 // Directory for lock files
 define('LOCK_DIR', TEMP_DIR);
 
 // Global from is %wwwDir% taken and is not set when php-cgi (run from cmd)
-$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../www/index.php';
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../../www/index.php';
 
 Tester\Environment::setup();
 
 $configurator = new Nette\Configurator;
 //$configurator->setDebugMode(TRUE);
-//$configurator->enableDebugger(__DIR__ . '/../log');
+//$configurator->enableDebugger(__DIR__ . '/../../log');
 $configurator->setTempDirectory(TEMP_DIR);
 $configurator->createRobotLoader()
-		->addDirectory(__DIR__ . '/../app')
-		->addDirectory(__DIR__ . '/../tests')
-		->addDirectory(__DIR__ . '/../vendor/others')
+		->addDirectory(__DIR__ . '/../../app')
+		->addDirectory(__DIR__ . '/../../tests')
+		->addDirectory(__DIR__ . '/../../vendor/others')
 		->register();
 
-$configurator->addConfig(__DIR__ . '/../app/config/config.neon');
-$configurator->addConfig(__DIR__ . '/../app/config/test/config.test.local.neon');
+$configurator->addConfig(__DIR__ . '/../../app/config/config.neon');
+$configurator->addConfig(__DIR__ . '/../../app/config/test/config.test.local.neon');
 
 return $configurator->createContainer();
