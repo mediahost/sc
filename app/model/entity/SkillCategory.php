c66545dba287057ba0cba971f051d8c663ec4705
Petr Poupě <petr.poupe@gmail.com>
2014-10-30 13:42:50 +0100

    code standards formating

--- app/model/entity/SkillCategory.php
+++ app/model/entity/SkillCategory.php
@@ -15,38 +15,35 @@ use Doctrine\ORM\Mapping as ORM,
  */
 class SkillCategory extends \Kdyby\Doctrine\Entities\BaseEntity
 {
-	
+
 	use \Kdyby\Doctrine\Entities\Attributes\Identifier;
-	
-	// <editor-fold defaultstate="collapsed" desc="constants & variables">
-	
+
+	// <editor-fold defaultstate="expanded" desc="constants & variables">
+
 	/**
 	 * @ORM\Column(type="string", nullable=false)
 	 */
 	protected $name;
-	
+
 	/**
 	 * @ORM\ManyToOne(targetEntity="SkillCategory", mappedBy="childs")
 	 * @ORM\JoinColumn(name="parent_id", referencedColumnName="id")
 	 */
 	protected $parent;
-	
+
 	/**
 	 * @ORM\OneToMany(targetEntity="SkillCategory", mappedBy="parent")
 	 */
 	protected $childs;
-	
+
 	// </editor-fold>
-	
-	// <editor-fold defaultstate="collapsed" desc="functions">
-	
+	// <editor-fold defaultstate="expanded" desc="functions">
+
 	public function __construct()
 	{
 		parent::__construct();
 		$this->childs = new ArrayCollection();
 	}
-	
-	// </editor-fold>
 
-	
+	// </editor-fold>
 }
