740fd5ba1c460ae1669402c67dc9dd8f4fc7b2a9
Samo Lisý <g.vvoody@gmail.com>
2014-11-05 14:41:24 +0100

    test bat files do clearing before testing

--- bin/testOne.example.bat
+++ bin/testOne.example.bat
@@ -1,5 +1,19 @@
 set this=%CD%
 set root=%this%/..
+cd /d "%root%/temp"
+del "btfj.dat"
+rd /s /q cache
+rd /s /q proxies
+rd /s /q install
+cd /d "%this%"
+
+cd /d "%root%/tests/temp"
+del "btfj.dat"
+rd /s /q cache
+cd /d "%root%/tests"
+del "test.log"
+cd /d "%this%"
+
 set phpIni="c:/xampp/php/php.ini"
 set testDir="%root%/tests/src/"
 set testFile="%root%/tests/src/extensions/Installer/Installer.phpt"
