3b2d8078b81c6c8372a878a6152da5f5edc1db69
Petr Poupě <petr.poupe@gmail.com>
2014-12-10 10:14:13 +0100

    loading button for connect manager

--- www/assets/scripts/custom/app-content.js
+++ www/assets/scripts/custom/app-content.js
@@ -7,10 +7,18 @@ var AppContent = function () {
 		});
 	};
 
+	var handleLoadingButton = function () {
+		$('.loading-btn').click(function (e) {
+			var btn = $(this);
+			btn.button('loading');
+		});
+	};
+
 	return {
 		//main function to initiate the module
 		init: function () {
 			handleExtendedTabs();
+			handleLoadingButton();
 		}
 	};
 
