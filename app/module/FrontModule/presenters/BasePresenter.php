840a01ec2ea52043c186c3c1f4433e54a27d3e50
Petr Poupě <petr.poupe@gmail.com>
2014-12-10 10:24:49 +0100

    pace off on front

--- app/module/FrontModule/presenters/BasePresenter.php
+++ app/module/FrontModule/presenters/BasePresenter.php
@@ -4,5 +4,11 @@ namespace App\FrontModule\Presenters;
 
 abstract class BasePresenter extends \App\BaseModule\Presenters\BasePresenter
 {
-	
+
+	protected function beforeRender()
+	{
+		parent::beforeRender();
+		$this->template->pacePluginOff = TRUE;
+	}
+
 }
