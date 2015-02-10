c48c446633a835751a46624a9ba129d25f5a7ca1
Petr Poupě <petr.poupe@gmail.com>
2015-02-10 14:16:11 +0100

    extended clear

--- bin/clear.bat
+++ bin/clear.bat
@@ -1,5 +1,6 @@
 set this=%CD%
 set root=%this%/..
+
 cd /d "%root%/temp"
 del "btfj.dat"
 rd /s /q cache
@@ -7,10 +8,21 @@ rd /s /q proxies
 rd /s /q install
 cd /d "%this%"
 
-cd /d "%root%/tests/temp"
-del "btfj.dat"
-rd /s /q cache
+cd /d "%root%/www/webtemp"
+del *.css
+del *.js
+cd /d "%this%"
+
 cd /d "%root%/tests"
 del "test.log"
 cd /d "%this%"
+
+cd /d "%root%/tests/temp"
+del "btfj.dat"
+del lock*
+rd /s /q cache
+rd /s /q proxies
+rd /s /q install
+cd /d "%this%"
+
 exit
