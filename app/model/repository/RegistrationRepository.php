e502a4df89fd2973550b733faff03a03232f4d5e
Petr Poupě <pupe.dupe@gmail.com>
2015-03-12 10:05:42 +0100

    Registration Repository test

--- app/model/repository/RegistrationRepository.php
+++ app/model/repository/RegistrationRepository.php
@@ -7,7 +7,7 @@ class RegistrationRepository extends BaseRepository
 
 	public function deleteByMail($mail)
 	{
-		return $this->createQueryBuilder()
+		return (bool) $this->createQueryBuilder()
 				->delete($this->getEntityName(), 'r')
 				->where('r.mail = ?1')
 				->setParameter(1, $mail)
