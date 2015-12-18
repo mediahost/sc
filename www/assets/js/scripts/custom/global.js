6c3b7ee87ad8fd95be13e87a889390c268c437ed
Petr Poupě <pupe.dupe@gmail.com>
2015-12-18 09:12:47 +0100

    candidate profile editor

--- www/assets/js/scripts/custom/global.js
+++ www/assets/js/scripts/custom/global.js
@@ -10,11 +10,47 @@ var Global = function () {
 		}
 	};
 
+	var handleSocialLinks = function () {
+		$(document).on('mouseover', '.social-editor .data', function (e) {
+			$(this).find('a.edit:not(.empty)').show();
+		});
+		$(document).on('mouseout', '.social-editor .data', function (e) {
+			$(this).find('a.edit:not(.empty)').hide();
+		});
+		$(document).on('click', '.social-editor a.edit', function (e) {
+			e.preventDefault();
+			$('.social-editor .data').hide();
+			$('.social-editor .editor').show();
+		});
+		$(document).on('click', '.social-editor input[type=submit]', function (e) {
+			e.preventDefault();
+			$('.social-editor .data').show();
+			$('.social-editor .editor').hide();
+		});
+	};
+
+	var handlePersonalDetails = function () {
+		var main = $('.personal-detail-editor .profile');
+		$(document).on('click', '.personal-detail-editor a.edit', function (e) {
+			e.preventDefault();
+			var target = $(this).attr('href');
+			$(target).show();
+			$(main).hide();
+		});
+		$(document).on('click', '.personal-detail-editor input[type=submit]', function (e) {
+			e.preventDefault();
+			$(main).show();
+			$('.personal-detail-editor .profile-edit').hide();
+		});
+	};
+
 	return {
 		init: function () {
 			$(document).ready(function () {
 				handleInitPickers();
 				handleHideRightbar();
+				handleSocialLinks();
+				handlePersonalDetails();
 			});
 		}
 	};
