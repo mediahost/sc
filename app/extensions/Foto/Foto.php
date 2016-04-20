3e5e23abd87ba7c5dce6d2193017a297100a56d2
stajo <stajo@post.sk>
2016-04-20 15:14:37 +0200

    messages - images resize

--- app/extensions/Foto/Foto.php
+++ app/extensions/Foto/Foto.php
@@ -80,7 +80,7 @@ class Foto extends Object
 
 		$sizeX = 0;
 		$sizeY = 0;
-		$resizeMethod = Image::FIT;
+		$resizeMethod = Image::EXACT;
 		if (preg_match('@^(\d+)' . preg_quote(FotoHelpers::getSizeSeparator()) . '(\d+)$@', $size, $matches)) {
 			$sizeX = $matches[1];
 			$sizeY = $matches[2];
