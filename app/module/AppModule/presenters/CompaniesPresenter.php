07e579ae2ab697cb64eca54d21d38ad375a0a083
stajo <stajo@post.sk>
2016-07-02 19:25:12 +0200

    CompanyInfoControl - refactoring

--- app/module/AppModule/presenters/CompaniesPresenter.php
+++ app/module/AppModule/presenters/CompaniesPresenter.php
@@ -9,8 +9,6 @@ use App\Components\Company\ICompanyInfoControlFactory;
 use App\Components\Company\ICompanyDataViewFactory;
 use App\Components\Grids\Company\CompaniesGrid;
 use App\Components\Grids\Company\ICompaniesGridFactory;
-use App\Components\User\CompanyUserControl;
-use App\Components\User\ICompanyUserControlFactory;
 use App\Model\Entity\Company;
 use App\Model\Entity\User;
 use App\Model\Facade\CompanyFacade;
@@ -37,10 +35,7 @@ class CompaniesPresenter extends BasePresenter
 
 	/** @var ICompanyImagesControlFactory @inject */
 	public $iCompanyImagesControlFactory;
-
-	/** @var ICompanyUserControlFactory @inject */
-	public $iCompanyUserControlFactory;
-
+    
 	/** @var ICompaniesGridFactory @inject */
 	public $iCompaniesGridFactory;
 	
@@ -107,7 +102,6 @@ class CompaniesPresenter extends BasePresenter
 			$this->redirect('default');
 		} else {
 			$this['companyInfoForm']->setCompany($this->company);
-			$this['editUserForm']->setCompany($this->company);
 		}
 	}
 
@@ -161,31 +155,6 @@ class CompaniesPresenter extends BasePresenter
 		$this->redirect('default');
 	}
 
-	/**
-	 * @secured
-	 * @resource('companies')
-	 * @privilege('editUser')
-	 */
-	public function actionEditUser($userId = NULL, $companyId = NULL)
-	{
-		if ($userId) {
-			$user = $this->userDao->find($userId);
-			if ($user) {
-				$this['editUserForm']->setUser($user);
-			}
-		}
-		if ($companyId) {
-			$company = $this->companyDao->find($companyId);
-			if ($company) {
-				$this['editUserForm']->setCompany($company);
-			}
-		}
-		$this['editUserForm']->onAfterSave = function (User $saved) {
-			$message = new TaggedString('User \'%s\' was successfully saved.', (string) $saved);
-			$this->flashMessage($message, 'success');
-			$this->redirect('default');
-		};
-	}
 
 	// </editor-fold>
 	// <editor-fold desc="forms">
@@ -194,9 +163,7 @@ class CompaniesPresenter extends BasePresenter
 	public function createComponentCompanyInfoForm()
 	{
 		$control = $this->iCompanyInfoControlFactory->create();
-		$control->setCanEditInfo($this->user->isAllowed('company', 'edit'));
-		$control->setCanEditUsers($this->user->isAllowed('company', 'edit'));
-		$control->setLinkAddUser($this->link('this#addUser'), ['data-toggle' => 'modal']);
+        $control->setUser($this->user);
 		$control->onAfterSave = function (Company $saved) {
 			$message = new TaggedString('Company \'%s\' was successfully saved.', (string) $saved);
 			$this->flashMessage($message, 'success');
@@ -214,18 +181,6 @@ class CompaniesPresenter extends BasePresenter
 			$this->flashMessage($message, 'success');
 			$this->redirect('this');
 //			$this->redirect('default');
-		};
-		return $control;
-	}
-
-	/** @return CompanyUserControl */
-	public function createComponentEditUserForm()
-	{
-		$control = $this->iCompanyUserControlFactory->create();
-		$control->onAfterSave = function (User $saved) {
-			$message = new TaggedString('User \'%s\' was successfully saved.', (string) $saved);
-			$this->flashMessage($message, 'success');
-			$this->redirect('this');
 		};
 		return $control;
 	}
