7f2dc1da811d34feae78ba4e8dd2e701649f5d47
stajo <stajo@post.sk>
2016-09-23 08:50:14 +0200

    grid control redesign

--- app/components/User/UsersGrid/UsersGrid.php
+++ app/components/User/UsersGrid/UsersGrid.php
@@ -20,12 +20,12 @@ class UsersGrid extends BaseControl
 	{
 		$grid = new BaseGrid();
 		$grid->setTranslator($this->translator);
-		$grid->setTheme(BaseGrid::THEME_METRONIC);
+		$grid->setTheme(BaseGrid::THEME_SUPR);
 
 		$repo = $this->em->getRepository(User::getClassName());
 		$qb = $repo->createQueryBuilder('u')
-				->select('u, r')
-				->leftJoin('u.roles', 'r');
+			->select('u, r')
+			->leftJoin('u.roles', 'r');
 		$grid->model = new Doctrine($qb, [
 			'roles' => 'r.id'
 		]);
@@ -35,50 +35,46 @@ class UsersGrid extends BaseControl
 			'mail' => 'ASC'
 		]);
 
-		$grid->addColumnNumber('id', 'ID #')
-				->setSortable()
-				->setFilterNumber();
-		$grid->getColumn('id')->headerPrototype->width = '5%';
+		$col = $grid->addColumnNumber('id', '#');
+		$col->setSortable()->setFilterNumber();
+		$col->getHeaderPrototype()->style['width'] = '5%';
+		$col->cellPrototype->class[] = 'center';
 
-		$grid->addColumnEmail('mail', 'Mail')
-				->setSortable()
-				->setFilterText()
-				->setSuggestion();
+		$col = $grid->addColumnEmail('mail', 'Mail');
+		$col->setSortable()->setFilterText()->setSuggestion();
+		$col->getHeaderPrototype()->style['width'] = '50%';
 
-		$roleRepo = $this->em->getRepository(Role::getClassName());
-		$grid->addColumnText('roles', 'Roles')
-				->setSortable()
-				->setFilterSelect($roleRepo->findPairs('name'));
-		$grid->getColumn('roles')
-				->setCustomRender(__DIR__ . '/tag.latte')
-				->setCustomRenderExport(function ($item) {
-					return Helpers::concatStrings(', ', $item->roles);
-				});
+		$col = $grid->addColumnText('roles', 'Roles');
+		$col->setSortable()
+			->setFilterSelect($this->getRoles());
+		$col->setCustomRender(__DIR__ . '/tag.latte')
+			->setCustomRenderExport([$this, 'joinRoles']);
+		$col->getHeaderPrototype()->style['width'] = '15%';
 
 		$grid->addActionHref('access', 'Access')
-						->setIcon('fa fa-key')
-						->setDisable(function($item) {
-							return !$this->presenter->userFacade->canAccess($this->identity, $item);
-						});
+			->setIcon('fa fa-key')
+			->setDisable(function ($item) {
+				return !$this->presenter->userFacade->canAccess($this->identity, $item);
+			});
 
 		$grid->addActionHref('edit', 'Edit')
-						->setIcon('fa fa-edit')
-						->setDisable(function($item) {
-							return !$this->presenter->userFacade->canEdit($this->identity, $item);
-						});
+			->setIcon('fa fa-edit')
+			->setDisable(function ($item) {
+				return !$this->presenter->userFacade->canEdit($this->identity, $item);
+			});
 
 		$grid->addActionHref('delete', 'Delete')
-						->setIcon('fa fa-trash-o')
-						->setConfirm(function($item) {
-							$message = $this->translator->translate('Are you sure you want to delete \'%s\'?');
-							return sprintf($message, (string) $item);
-						})
-						->setDisable(function($item) {
-							return !$this->presenter->userFacade->canDelete($this->identity, $item);
-						})
-				->getElementPrototype()->class[] = 'red';
-						
-		$grid->setActionWidth("20%");
+			->setIcon('fa fa-trash-o')
+			->setConfirm(function ($item) {
+				$message = $this->translator->translate('Are you sure you want to delete \'%s\'?');
+				return sprintf($message, (string)$item);
+			})
+			->setDisable(function ($item) {
+				return !$this->presenter->userFacade->canDelete($this->identity, $item);
+			})
+			->getElementPrototype()->class[] = 'red';
+
+		$grid->setActionWidth("25%");
 
 		$grid->setExport('users');
 
@@ -91,6 +87,14 @@ class UsersGrid extends BaseControl
 		return $this;
 	}
 
+	private function getRoles() {
+		$roleRepo = $this->em->getRepository(Role::getClassName());
+		return $roleRepo->findPairs('name');
+	}
+
+	private function joinRoles($item) {
+		return Helpers::concatStrings(', ', $item->roles);
+	}
 }
 
 interface IUsersGridFactory
