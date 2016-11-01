0b13e83876c6c865d226706dc4b6925086c3d52e
Petr Poupě <pupe.dupe@gmail.com>
2016-11-01 11:03:36 +0100

    Restyled job list

--- app/components/Candidate/JobList/JobList.php
+++ app/components/Candidate/JobList/JobList.php
@@ -8,11 +8,19 @@ use App\Model\Entity\Job;
 class JobList extends BaseControl
 {
 
+	private $limit;
+
 	public function render()
 	{
 		$jobRepo = $this->em->getRepository(Job::getClassName());
-		$this->template->jobs = $jobRepo->findAll();
+		$this->template->jobs = $jobRepo->findBy([], [], $this->limit);
 		parent::render();
+	}
+
+	public function setLimit($value)
+	{
+		$this->limit = $value;
+		return $this;
 	}
 }
 
