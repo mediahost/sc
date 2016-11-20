0ec19d6639832478171d27dcf1948705aff16648
Petr Poupě <pupe.dupe@gmail.com>
2016-11-20 12:59:21 +0100

    Refactoring

--- app/model/facade/Job/JobFacade.php
+++ app/model/facade/Job/JobFacade.php
@@ -2,12 +2,11 @@
 
 namespace App\Model\Facade;
 
-
+use App\Model\Entity\Cv;
+use App\Model\Entity\Job;
 use App\Model\Entity\JobCategory;
 use App\Model\Entity\JobType;
 use App\Model\Facade\Traits\JobCategoryFacade;
-use App\Model\Entity\Cv;
-use App\Model\Entity\Job;
 use App\Model\Repository\CvRepository;
 use App\Model\Repository\JobRepository;
 use Kdyby\Doctrine\EntityManager;
@@ -15,18 +14,17 @@ use Nette\Object;
 
 class JobFacade extends Object
 {
-    use JobCategoryFacade;
+	use JobCategoryFacade;
 
 	/** @var EntityManager @inject */
 	public $em;
 
 	/** @var CvRepository */
 	private $cvRepo;
-	
+
 	/** @var JobRepository */
 	private $jobRepo;
 
-	
 	public function __construct(EntityManager $em)
 	{
 		$this->em = $em;
@@ -38,41 +36,42 @@ class JobFacade extends Object
 	{
 		return $this->cvRepo->findBySkillRequests($job->skillRequests->toArray());
 	}
-    
-    public function find($id) {
-        return $this->jobRepo->find($id);
-    }
-	
-	public function findJobType($idType) 
+
+	public function find($id)
+	{
+		return $this->jobRepo->find($id);
+	}
+
+	public function findJobType($idType)
 	{
 		$typeRepo = $this->em->getDao(JobType::getClassName());
 		return $typeRepo->find($idType);
 	}
-	
-	public function findOrCreateJobType($type) 
+
+	public function findOrCreateJobType($type)
 	{
 		$typeRepo = $this->em->getDao(JobType::getClassName());
 		$entity = $typeRepo->findOneBy(['name' => $type]);
-		if(!isset($entity)) {
+		if (!isset($entity)) {
 			$entity = new JobType();
 			$entity->name = $type;
 			$typeRepo->save($entity);
 		}
 		return $entity;
 	}
-	
+
 	public function getJobTypes()
 	{
 		$typeRepo = $this->em->getDao(JobType::getClassName());
 		return $typeRepo->findPairs('name');
 	}
-	
+
 	public function getJobCategories()
 	{
 		$categoryRepo = $this->em->getDao(JobCategory::getClassName());
 		return $categoryRepo->findPairs('name');
 	}
-	
+
 	public function delete($id)
 	{
 		$job = $this->jobRepo->find($id);
