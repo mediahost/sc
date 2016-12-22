558b0caaa4fde2f926add93c3397b94d57b631d8
Petr Poupě <pupe.dupe@gmail.com>
2016-12-22 11:42:02 +0100

    Add redirect parameter for sign out

--- app/components/Auth/SignOut/SignOut.php
+++ app/components/Auth/SignOut/SignOut.php
@@ -9,11 +9,14 @@ class SignOut extends BaseControl
 
 	const REDIRECT_AFTER_LOGOUT = ':Front:Homepage:';
 
-	public function handleSignOut()
+	public function handleSignOut($redirectUrl = NULL)
 	{
 		$this->presenter->user->logout();
 		$message = $this->translator->translate('You have been successfuly signed out.');
 		$this->presenter->flashMessage($message, 'success');
+		if ($redirectUrl) {
+			$this->presenter->redirectUrl($redirectUrl);
+		}
 		$this->presenter->redirect(self::REDIRECT_AFTER_LOGOUT);
 	}
 
