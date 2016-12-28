a9296b7e2007c0d15b44279a73f8360c223e2b11
Petr Poupě <pupe.dupe@gmail.com>
2016-12-28 10:42:45 +0100

    Redirecting on localhost

--- app/module/FrontModule/presenters/HomepagePresenter.php
+++ app/module/FrontModule/presenters/HomepagePresenter.php
@@ -9,6 +9,8 @@ class HomepagePresenter extends BasePresenter
 	{
 		if ($this->user->loggedIn) {
 			$this->redirect(':App:Dashboard:');
+		} else if ($this->getHttpRequest()->getRemoteAddress() == '127.0.0.1') {
+			$this->redirect('Sign:in');
 		} else {
 			$this->redirectUrl('/wp/');
 		}
