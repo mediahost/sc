bd77784b868bbd4f4e289ea1bce4aa7abacbf200
Petr Poupě <pupe.dupe@gmail.com>
2016-12-28 11:32:49 +0100

    CV wording

--- app/components/AfterRegistration/CompleteAccount/CompleteCv.php
+++ app/components/AfterRegistration/CompleteAccount/CompleteCv.php
@@ -42,7 +42,7 @@ class CompleteCv extends BaseControl
 			'application/msword',
 			'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
 		];
-		$form->addUpload('cvFile', 'Your CV')
+		$form->addUpload('cvFile', 'Upload New CV')
 			->addRule(Form::MIME_TYPE, 'File must be PDF or DOC', implode(',', $acceptedFiles));
 
 		$form->addHidden('jobApplyId');
