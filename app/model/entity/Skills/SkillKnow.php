701c839c76bd3770a90a1bbd49784862fe9f679e
stajo <stajo@post.sk>
2017-01-12 07:43:59 +0100

    Fixed crash

--- app/model/entity/Skills/SkillKnow.php
+++ app/model/entity/Skills/SkillKnow.php
@@ -48,7 +48,7 @@ class SkillKnow extends BaseEntity
 
 	public function isEmpty()
 	{
-		return (bool) (!$this->level->isRelevant());
+		return (bool) (!$this->level  ||  !$this->level->isRelevant());
 	}
 
 	public function hasYearsInRange($from = NULL, $to = NULL)
