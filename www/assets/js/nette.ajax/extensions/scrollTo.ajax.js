19c11b385661201796b365090b06d27259d652f5
Petr Poupě <pupe.dupe@gmail.com>
2017-03-23 09:21:14 +0100

    Move page after load snippet

--- www/assets/js/nette.ajax/extensions/scrollTo.ajax.js
+++ www/assets/js/nette.ajax/extensions/scrollTo.ajax.js
@@ -5,10 +5,16 @@
 	 */
 	$.nette.ext('scrollTo', {
 		init: function () {
-			this.ext('snippets', true).before($.proxy(function ($el) {
-				if ($($el).hasClass('scrollHere')) {
-					var offset = $el.offset();
-					scrollTo(offset.left, offset.top);
+			this.ext('snippets', true).before($.proxy(function (el) {
+				var $el = $(el);
+				if ($el.hasClass('scrollHere')) {
+					var $target = $el;
+					if ($el.data('target')) {
+						$target = $($el.data('target'));
+					}
+					var offset = $target.offset();
+
+					scrollTo(offset.left, offset.top - 60 - 20);
 					this.shouldTry = false;
 				}
 			}), this);
